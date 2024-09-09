1d6c585d6d74d90d6629bc6bbd37b8d786a77140
levy.buarque <levy.buarque@gmail.com>
2024-09-09 15:38:56 -0300

    fix: adjustments of scripts

--- spec/isbnSpec.js
+++ spec/isbnSpec.js
@@ -1,23 +1,30 @@
-const { isValidISBN10 } = require('./isbn.js'); 
+const isValidISBN10 = require('./js/isbn.js')
 
 describe("isValidISBN10", function() {
+
   it("deve retornar true para um ISBN-10 válido sem hífens", function() {
-    expect(isValidISBN10("0471958697")).toBe(true);
+    const isbn = "0471958697";
+    expect(isValidISBN10(isbn)).toBe(true);
   });
 
   it("deve retornar true para um ISBN-10 válido com hífens", function() {
-    expect(isValidISBN10("0-471-95869-7")).toBe(true);
+    const isbn = "0-471-95869-7";
+    expect(isValidISBN10(isbn)).toBe(true);
   });
 
   it("deve retornar false para um ISBN-10 inválido com dígito de verificação incorreto", function() {
-    expect(isValidISBN10("0471958698")).toBe(false);
+    const isbn = "0471958698";
+    expect(isValidISBN10(isbn)).toBe(false);
   });
 
   it("deve retornar false para um ISBN-10 com comprimento incorreto", function() {
-    expect(isValidISBN10("123456789")).toBe(false);
+    const isbn = "123456789";
+    expect(isValidISBN10(isbn)).toBe(false);
   });
 
   it("deve retornar true para um ISBN-10 válido com 'X' como dígito de verificação", function() {
-    expect(isValidISBN10("012000030X")).toBe(true);
+    const isbn = "012000030X";
+    expect(isValidISBN10(isbn)).toBe(true);
   });
+
 });
--- spec/js/isbn.js
+++ spec/js/isbn.js
@@ -4,15 +4,15 @@ function isValidISBN10(isbn) {
     return false;
   }
   let sum = 0;
-  for (let i = 0; 9 > i; i++) {
-    if (0 > isbn[i] || isbn[i] > 9) {
+  for (let i = 0; i < 9; i++) {
+    if (isbn[i] < 0 || isbn[i] > 9) {
       return false;
     }
-    sum += (isbn[i] * (10- i));
+    sum += (isbn[i] * (10 - i));
   }
   let checksum = isbn[9].toUpperCase();
   sum += (checksum === 'X') ? 10 : parseInt(checksum);
   return (sum % 11 === 0);
 }
 
-module.exports = isValidISBN10;
+module.exports = isValidISBN10;  
